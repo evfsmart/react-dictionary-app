f35d2120e5a4d383d3f866f1471a9279d7fd7b5f
evfsmart <evfsmart@gmail.com>
2021-06-13 21:20:52 +0100

    Rendered the Meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    return console.log(response.data[0].meanings);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -46,6 +48,7 @@ export default function Dictionary() {
           </div>
         </div>
       </form>
+      <Results results={results} />
     </div>
   );
 }
