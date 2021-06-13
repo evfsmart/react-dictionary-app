0c5d3afc378b8a73e8126e62aed96b9ca52fea36
evfsmart <evfsmart@gmail.com>
2021-06-13 16:56:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img
+          src="/img/book-icon-512px.png"
+          alt="book logo"
+          className="App-logo"
+        />
+        <h1>Dictionary App</h1>
+        <br />
+        <button className="btn btn-primary">Search</button>
+      </div>
+    </div>
+  );
+}
